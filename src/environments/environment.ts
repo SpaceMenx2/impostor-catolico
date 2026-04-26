04cdb1a8c417baaf9ae9cd3dc94a7d38fa7e3139
Spacemenx2 <joaalesand@gmail.com>
2026-04-26 16:24:22 -0300

    Subiendo versión 1.1.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appVersion: '1.1.0', // ← Actualizar manualmente al hacer release
+  appVersion: '1.1.1', // ← Actualizar manualmente al hacer release
   githubOwner: 'SpaceMenx2',
   githubRepo: 'impostor-catolico'
 };
